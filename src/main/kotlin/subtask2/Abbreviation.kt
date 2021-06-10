f3c0985ecb5ee4928fa6a956f166a9b56134ebd8
viktorlysenko <viktor.lysenco@gmail.com>
2021-06-10 23:17:59 +0300

    feat: add date format task

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,6 +4,15 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+
+        var aUpperCase = a.toUpperCase()
+        var index = 0
+        while(index <= aUpperCase.length - 1) {
+            
+            index++
+        }
+
+        return "YES"
     }
 }
