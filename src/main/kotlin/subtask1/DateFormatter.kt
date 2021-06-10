175c3c8f9db74584b7daa3ef4d59062435fd9f4d
viktorlysenko <viktor.lysenco@gmail.com>
2021-06-10 23:06:57 +0300

    feat: add date format task

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,29 @@
 package subtask1
 
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        return try {
+            val inputFormat:SimpleDateFormat = SimpleDateFormat(INPUT_DATE_FORMAT)
+            inputFormat.isLenient = false
+            val taskDate: Date = inputFormat.parse("$day.$month.$year")
+
+            val newFormat: SimpleDateFormat = SimpleDateFormat(OUTPUT_DATE_FORMAT,Locale("RU"))
+
+            newFormat.format(taskDate)
+        }catch (e: ParseException){
+            ERROR_DATE
+        }
+
+    }
+
+    companion object{
+        private const val ERROR_DATE :String = "Такого дня не существует"
+        private const val INPUT_DATE_FORMAT :String = "dd.MM.yyyy"
+        private const val OUTPUT_DATE_FORMAT :String = "dd MMMM, EEEE"
     }
 }
